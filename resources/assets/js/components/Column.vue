4cd3d31ffbf47923295ccc03b7452d8148b68f93
bausano <gtasampserver@seznam.cz>
2017-09-06 03:32:10 +0100

    Styling mostly

---
+++ resources/assets/js/components/Column.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="links">
+      <h2>{{ heading }}</h2>
+      <div class="link" v-for="link in links">
+        <div class="title">{{ link.title }}</div>
+        <div class="description">{{ truncate(link.description) }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  const _ = require('lodash')
+
+  export default {
+    props: ['heading'],
+    data() {
+      return {
+        links: [
+          {
+            title: 'Dante\'s hell #3: Eval',
+            description: 'Accusamus tempore magnam totam earum quis quisquam. Rerum laboriosam cum voluptatum voluptatibus, fugit asperiores!',
+            id: '431'
+          },
+          {
+            title: 'Dante\'s hell #3: Eval',
+            description: 'Rerum laboriosam cum voluptatum voluptatibus, fugit asperiores!',
+            id: '433'
+          }
+        ]
+      }
+    },
+
+    methods: {
+      truncate: (string) => {
+        return _.truncate(string, {
+          length: 70
+        })
+      }
+    }
+  }
+</script>
---
+++ resources/assets/js/components/Heel.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="heel row">
+    <div class="social col-md-6">
+      <div class="link">
+        <a href="http://github.com/bausano/satan-codes" target="_blank">
+          <i class="fa fa-github fa-lg"></i>
+          github.com/bausano/satan-codes
+        </a>
+      </div>
+      <div class="link">
+        <a href="https://twitter.com/BausanoMichael" target="_blank">
+          <i class="fa fa-twitter fa-lg"></i>
+          twitter.com/BausanoMichael
+        </a>
+      </div>
+      <div class="link">
+        <a href="https://www.linkedin.com/in/bausano/" target="_blank">
+          <i class="fa fa-linkedin fa-lg"></i>
+          linkedin.com/in/bausano/
+        </a>
+      </div>
+
+      <div class="link" v-on:click="selectMe()">
+        <i class="fa fa-envelope fa-lg"></i>
+        <input class="select-me"
+          value="bausanomichal@gmail.com"
+          readonly/>
+      </div>
+
+      <div class="copyright">I ♥ memes &copy; Michael Bausano 2017</div>
+    </div>
+    <div class="disclaimer col-md-6">
+      <div class="link">
+        <a href="#" target="_blank">
+          <i class="fa fa-sitemap fa-lg"></i>
+          List all scrolls
+        </a>
+      </div>
+      <p>
+        This blog is a sarcastic project of
+        <a href="http://bausano.me" target="_blank">mine</a> and if you don't
+        find it fun then probably because it's not. But hey, you can always
+        tell me how much my humour sucks at
+        <strong>bausanomichal&commat;gmail.com</strong>.
+      </p>
+      <p>
+        If you'd like to submit some code you can get in touch with me
+        via any <strong>social media</strong> or <strong>email</strong>.
+      </p>
+      <p><a>#desecratemycode</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      selectMe: () => {
+        $('.select-me').select()
+      }
+    }
+  }
+</script>
